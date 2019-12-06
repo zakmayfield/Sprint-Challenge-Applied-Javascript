0982552696c56c333245c135a402e6d97d4fc1f8
Zachary Mayfield <zakmayfield@gmail.com>
2019-12-06 12:23:34 -0500

    finished mvp minus questions

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,33 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+//header component
+
+const headerContainer = document.querySelector('.header-container');
+
+const headerComp = () => {
+  //Create Elements
+  const header = document.createElement('div'),
+        date = document.createElement('span'),
+        title = document.createElement('h1'),
+        temp = document.createElement('span');
+  
+  //classes and attrs
+  header.classList.add('header');
+  date.classList.add('date');
+  temp.classList.add('temp');
+  
+  //text content
+  date.textContent = 'MARCH 28, 2019';
+  title.textContent = 'Lambda Times';
+  temp.textContent = '98 degrees';
+  
+  //appending
+  header.appendChild(date);
+  header.appendChild(title);
+  header.appendChild(temp);
+  
+  return header;
+};
+
+headerContainer.appendChild(headerComp());
